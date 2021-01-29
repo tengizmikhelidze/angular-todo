dbf9788d26bee8ce1955dbb51e5a08768fdc313d
Tengiz Mikhelidze <mixelidzet@gmail.com>
2021-01-29 23:04:37 +0400

    fix(files): fixed files structure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Todolist
+# AngularTodolist
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todolist'`, () => {
+  it(`should have as title 'angular-todolist'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todolist');
+    expect(app.title).toEqual('angular-todolist');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('todolist app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-todolist app is running!');
   });
 });
